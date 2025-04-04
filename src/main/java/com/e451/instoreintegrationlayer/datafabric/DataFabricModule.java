3c93eda207798a7f02602989f661fc09b208a093
Sridhar Redreddy <sridhar.redreddy@8451.com>
2025-04-04 15:38:38 -0400

    removed datafabric module

--- src/main/java/com/e451/instoreintegrationlayer/datafabric/DataFabricModule.java
+++
@@ -1,7 +0,0 @@
-package com.e451.instoreintegrationlayer.datafabric;
-
-import org.springframework.modulith.ApplicationModule;
-
-@ApplicationModule(displayName = "Data Fabric Integration")
-public class DataFabricModule {
-}
--- src/main/java/com/e451/instoreintegrationlayer/datafabric/service/DataFabricService.java
+++
@@ -1,8 +0,0 @@
-package com.e451.instoreintegrationlayer.datafabric.service;
-
-import com.e451.instoreintegrationlayer.webhook.dto.EventPayloadDto;
-import reactor.core.publisher.Mono;
-
-public interface DataFabricService {
-    Mono<Void> syncCampaign(EventPayloadDto eventPayload);
-}
